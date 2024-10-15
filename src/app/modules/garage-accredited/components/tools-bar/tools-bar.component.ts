0ca81727144b58801fbec4ccf2d48f8612f628d7
HugoJSG <hugososageisse@gmail.com>
2024-10-15 20:44:54 -0300

    começo da barra de tarefas

--- src/app/modules/garage-accredited/components/tools-bar/tools-bar.component.ts
+++ src/app/modules/garage-accredited/components/tools-bar/tools-bar.component.ts
@@ -1,17 +1,23 @@
-import { NgClass } from "@angular/common";
+import { NgClass, NgIf } from "@angular/common";
 import { Component } from "@angular/core";
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: "app-tools-bar",
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, NgIf, HeaderComponent],
   templateUrl: "./tools-bar.component.html",
   styleUrl: "./tools-bar.component.scss",
 })
 export class ToolsBarComponent {
   open: boolean = false;
+  openMeusLocais: boolean = false;
 
   abrir() {
     this.open = !this.open;
+  }
+
+  meusLocais() {
+    this.openMeusLocais = !this.openMeusLocais;
   }
 }
